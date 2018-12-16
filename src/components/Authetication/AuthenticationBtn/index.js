1cde8a99a560c26ed6ad474c643a09d434e76da6
Vitoria Heliane <vitoriaheliane@gmail.com>
2018-12-16 19:49:48 -0300

    Add logout functionality

--- src/components/Authetication/AuthenticationBtn/index.js
+++ src/components/Authetication/AuthenticationBtn/index.js
@@ -6,30 +6,46 @@ import { withRouter, Link } from 'react-router-dom'
 import './global.css'
 
 class AuthenticationBtn extends Component {
-    static propTypes = {
-        cookies: instanceOf(Cookies).isRequired,
-        history: PropTypes.shape({
-            push: PropTypes.func.isRequired
-        })
-    }
+  static propTypes = {
+    cookies: instanceOf(Cookies).isRequired,
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired
+    })
+  }
 
-    isLogged = () => this.props.cookies.get('ccc-pharma-token')
+  isLogged = () => this.props.cookies.get('ccc-pharma-token')
 
-    logout = () => {
-        const { cookies } = this.props
-        cookies.set('ccc-pharma-token', '')
-        window.location.reload()
-    }
+  logout = () => {
+    const { cookies } = this.props
+    cookies.set('ccc-pharma-token', '')
+    window.location.reload()
+  }
 
-    render() {
-        return (
-            <div id="authBtn">{
-                this.isLogged() ?
-                <i className="fas fa-sign-out-alt" id="logoutBtn" onClick={this.logout}><span>Sair</span></i>
-                : <Link to={{ pathname: "/login" }}><i className="fas fa-user"><span>Entrar</span></i></Link>
-            }</div>
-        )
-    }
+  render() {
+    return (
+      <div id="authBtn">
+        {
+          this.isLogged() ? (
+            <Link to={{ pathname: "/" }}>
+              <i
+                className="fas fa-sign-out-alt"
+                id="logoutBtn"
+                onClick={this.logout}
+              >
+                <span>Sair</span>
+              </i>
+            </Link>
+          ) : (
+              <Link to={{ pathname: "/login" }}>
+                <i className="fas fa-user">
+                  <span>Entrar</span>
+                </i>
+              </Link>
+            )
+        }
+      </div>
+    )
+  }
 }
 
 export default withRouter(withCookies(AuthenticationBtn))
