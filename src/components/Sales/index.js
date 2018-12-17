967c9e7cebde45ff4f5996275417398296fa42d8
Vitoria Heliane <vitoriaheliane@gmail.com>
2018-12-17 12:17:18 -0300

    Finishh project

--- src/components/Sales/index.js
+++ src/components/Sales/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import animation from '../../asserts/animation1.gif'
 
 class Sales extends Component {
     render() {
         return (
-            <div>
-                Sales...
+            <div className="flex-column">
+                <div><img src={animation}></img></div>
+                <span>A culpa é do back...</span>
             </div>
         )
     }
