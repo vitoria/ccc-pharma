86542a61c9ea9ee1a1340eae431e0286282a9134
Vitoria Heliane <vitoriaheliane@gmail.com>
2018-12-17 05:07:34 -0300

    Add logged user name at the header

--- src/components/Authetication/AuthenticationBtn/index.js
+++ src/components/Authetication/AuthenticationBtn/index.js
@@ -1,16 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { instanceOf, PropTypes } from 'prop-types'
 import { withCookies, Cookies } from 'react-cookie'
 import { withRouter, Link } from 'react-router-dom'
 
+import { getCurrentUser } from '../../../utils'
+
 import './global.css'
 
 class AuthenticationBtn extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      profile: false
+    }
+  }
+
   static propTypes = {
     cookies: instanceOf(Cookies).isRequired,
     history: PropTypes.shape({
       push: PropTypes.func.isRequired
     })
+  }
+
+  componentDidUpdate = () => {
+    getCurrentUser(this.isLogged())
+      .then(response => {
+        if (response.status === 200) {
+          response.json().then(profile => {
+            this.setState({ profile: profile })
+          })
+        }
+      })
   }
 
   isLogged = () => this.props.cookies.get('ccc-pharma-token')
@@ -22,28 +42,38 @@ class AuthenticationBtn extends Component {
   }
 
   render() {
+    const { profile } = this.state
     return (
-      <div id="authBtn">
-        {
-          this.isLogged() ? (
-            <Link to={{ pathname: "/" }}>
-              <i
-                className="fas fa-sign-out-alt"
-                id="logoutBtn"
-                onClick={this.logout}
-              >
-                <span>Sair</span>
-              </i>
-            </Link>
-          ) : (
-              <Link to={{ pathname: "/login" }}>
-                <i className="fas fa-user">
-                  <span>Entrar</span>
+      <Fragment>
+        {profile && (
+          <div id="user">
+            <i className="fas fa-user" />
+            <span> {profile.name}</span>
+            <span></span>
+          </div>
+        )}
+        <div id="authBtn">
+          {
+            this.isLogged() ? (
+              <Link to={{ pathname: "/" }}>
+                <i
+                  className="fas fa-sign-out-alt"
+                  id="logoutBtn"
+                  onClick={this.logout}
+                >
+                  <span>Sair</span>
                 </i>
               </Link>
-            )
-        }
-      </div>
+            ) : (
+                <Link to={{ pathname: "/login" }}>
+                  <i className="fas fa-user">
+                    <span>Entrar</span>
+                  </i>
+                </Link>
+              )
+          }
+        </div>
+      </Fragment>
     )
   }
 }
